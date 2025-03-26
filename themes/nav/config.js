e78a0b7b35fea1d62513d22466ad22887e62bc66
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2025-03-26 23:21:42 +0800

    Add files via upload

--- themes/nav/config.js
+++ themes/nav/config.js
@@ -15,6 +15,16 @@ const CONFIG = {
   // Widget
   NAV_WIDGET_REVOLVER_MAPS:
     process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'false', // 地图插件
-  NAV_WIDGET_TO_TOP: true // 跳回顶部
+  NAV_WIDGET_TO_TOP: true, // 跳回顶部
+
+  //nav来自heo显示封面配置
+  HEO_POST_LIST_COVER: true, // 列表显示文章封面
+  HEO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+
+  HEO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
+  HEO_HOME_POST_TWO_COLS: true, // 首页博客两列显示，若为false则只显示一列
+  HEO_POST_LIST_PREVIEW: false, // 读取文章预览
+
 }
+
 export default CONFIG
